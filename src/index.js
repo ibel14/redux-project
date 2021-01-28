78c19c0fb5f4ff399c3ff0e51443d0e868f33459
ponaehali <iambelousov@gmail.com>
2021-01-28 20:50:03 +0200

    #106 homework

--- src/index.js
+++ src/index.js
@@ -1,35 +1,35 @@
 import {createStore} from 'redux';
 
-const reducer = (state = 0, action) => {
+const counter = (state = 0, action) => {
   switch (action.type) {
     case 'INC':
       return state +1;
       case 'DEC':
         return state -1;
-        case 'RND':
-        return state + action.value;
+        case 'CLR':
+        return 0;
     default:
       return state;
   }
 }
 
+let store = createStore(counter);
+
 const inc = () => ({type: 'INC'});
 const dec = () => ({type: 'DEC'});
-const rnd = (value) => ({type: 'RND', value});
-
-const store = createStore(reducer);
+const clr = () => ({type: 'CLR'});
 
 document.getElementById('inc').addEventListener('click', () => {
-  store.dispatch(inc())
+  store.dispatch(inc());
 });
 
 document.getElementById('dec').addEventListener('click', () => {
   store.dispatch(dec());
 });
-document.getElementById('rnd').addEventListener('click', () => {
-  const value = Math.floor(Math.random() * 10);
-  store.dispatch(rnd(value));
+document.getElementById('clr').addEventListener('click', () => {
+  store.dispatch(clr());
 });
+
 
 const update = () => {
   document.getElementById('counter').textContent = store.getState();

58e52cc886f6e27fbecc8637410f7d53c5c08fd7
ponaehali <iambelousov@gmail.com>
2021-02-03 22:18:16 +0200

    #107 bindaction

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
-import {createStore} from 'redux';
+import {createStore, bindActionCreators} from 'redux';
 import reducer from './reducer';
 import {inc, dec, rnd} from './actions';
 
 const store = createStore(reducer);
 const {dispatch} = store;
 
-const incDispatch = () => dispatch(inc());
-const decDispatch = () => dispatch(dec());
+// const bindActionCreator = (creator, dispatch) => (...args) => {
+//   dispatch(creator(...args));
+// }
+
+const incDispatch = bindActionCreators(inc, dispatch);
+const decDispatch = bindActionCreators(dec, dispatch);
+const rndDispatch = bindActionCreators(rnd, dispatch);
+
 
 document.getElementById('inc').addEventListener('click', incDispatch);
-
 document.getElementById('dec').addEventListener('click', decDispatch);
 document.getElementById('rnd').addEventListener('click', () => {
   const value = Math.floor(Math.random() * 10);
-  dispatch(rnd(value));
+  rndDispatch(value)
 });
 
 

cecf1c8f82cbe6ec7f85c34f5d63ea1b02c40ff3
ponaehali <iambelousov@gmail.com>
2021-02-05 00:39:31 +0200

    #107 finally

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import * as actions from '../actions';
+// import {bindActionCreators} from 'redux';
 
 const Counter = (counter, inc, dec, rnd) => {
     return (
@@ -10,4 +13,10 @@ const Counter = (counter, inc, dec, rnd) => {
     </div>
     );
 }
-export default Counter;
+const mapStateToProps = (state) => {
+  return {
+    counter: state
+  }
+}
+
+export default connect(mapStateToProps, actions)(Counter);
